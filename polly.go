38000f94e9b6a337a1775f3789878469bb2f2668
buzzz321 <anders.olme@gmail.com>
2017-04-29 09:45:20 +0200

    renaming and enabling one more guild

--- polly.go
+++ polly.go
@@ -64,33 +64,35 @@ func guildCreate(s *discordgo.Session, mguild *discordgo.GuildCreate) {
 
 	if mguild.Name == "Notte_test" {
 		if dg != nil {
-			dg.ChannelMessageSend(guilds["256795736677679104"].Channels[0].ID, "Tjo!")
+			//dg.ChannelMessageSend(guilds["256795736677679104"].Channels[0].ID, "Tjo!")
+			//dg.ChannelMessageSend(guilds["95498187816570880"].Channels[0].ID, "Tjo!")
 		}
 	}
 	mutex.Unlock()
 }
 
 func runner() {
-	var kdRed, kdGreen, kDBlue float64 = 0.0, 0.0, 0.0
+	var redKD, greenKD, kDBlue float64 = 0.0, 0.0, 0.0
 	gw2 := gw2util.Gw2Api{BaseUrl: "https://api.guildwars2.com/v2/", Key: gw2util.GetUserData(userData, "Notimik").Key}
 	for {
-		time.Sleep(10 * time.Minute)
+		time.Sleep(20 * time.Minute)
 		stats := gw2util.GetWWWStats(gw2, "2007")
 
 		if stats.Kills.Blue > 0 {
 			kDBlue = stats.Kills.Blue / stats.Deaths.Blue
 		}
 		if stats.Kills.Red > 0 {
-			kdRed = stats.Kills.Red / stats.Deaths.Red
+			redKD = stats.Kills.Red / stats.Deaths.Red
 		}
 		if stats.Kills.Green > 0 {
-			kdGreen = stats.Kills.Green / stats.Deaths.Green
+			greenKD = stats.Kills.Green / stats.Deaths.Green
 		}
 
 		mutex.Lock()
-		msg := fmt.Sprintf("K/D \n Blue: %6.2f\n Red: %6.2f\n Green: %6.2f\n", kDBlue, kdRed, kdGreen)
+		msg := fmt.Sprintf("K/D \n Blue: %6.2f\n Red: %6.2f\n Green: %6.2f\n", kDBlue, redKD, greenKD)
 		fmt.Println(msg)
 		dg.ChannelMessageSend(guilds["256795736677679104"].Channels[0].ID, msg)
+		dg.ChannelMessageSend(guilds["95498187816570880"].Channels[0].ID, msg)
 		mutex.Unlock()
 
 	}
